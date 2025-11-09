d95450dd2dfa49407d25865e459e42229593fa16
Satyam Maurya <smauryachs44@gmail.com>
2025-11-09 20:40:07 +0530

    refactor: adjust padding in Header.css and fix typo in Header.jsx

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -8,11 +8,10 @@ const Header = () => {
         <h2>Order your favorite food here</h2>
         <p>
           Choose from a diverse menu featuring a delectable array of dishes
-          crafted with the finest ingredient and culinary expertise. Our mission
-          is to satisfy your craving and elevate your dining experience, one
-          delicious meal at a time.
+          crafted with the finest ingredients and culinary expertise. Our
+          mission is to satisfy your craving and elevate your dining experience,
+          one delicious meal at a time.
         </p>
-        <button>View Menu</button>
       </div>
     </div>
   );
