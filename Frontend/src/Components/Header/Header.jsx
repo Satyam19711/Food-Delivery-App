e7ff9faa2c07b40332434ff9087b0b39fe80e32c
Satyam Maurya <smauryachs44@gmail.com>
2025-11-09 20:11:25 +0530

    refactor: update image path in Header.css and fix typo in Header.jsx; remove unused react.svg

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header-contents">
-        <h2>Order your favourite food here</h2>
+        <h2>Order your favorite food here</h2>
         <p>
           Choose from a diverse menu featuring a delectable array of dishes
           crafted with the finest ingredient and culinary expertise. Our mission
